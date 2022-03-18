45b0b9538c14d0fb6377bb889849463c80d0227f
yellowsink <yellowsink@riseup.net>
2022-03-18 10:55:23 +0000

    start

---
+++ src/YamlValue.fs
@@ -0,0 +1,33 @@
+﻿module FSharp.Data
+
+open System
+open System.Collections.Generic
+
+[<RequireQualifiedAccess>]
+type YamlValue =
+  | String of string
+  | Number of decimal
+  //| Float of float
+  | Record of properties:(string * YamlValue)[]
+  | Array of elements:YamlValue[]
+  | Boolean of bool
+  | Null
+
+   static member private FromObjTree (node: obj) =
+    match node with
+    | :? string as str ->
+        match str with
+        | "true" -> YamlValue.Boolean true
+        | "false" -> YamlValue.Boolean false
+        | _ ->
+            match Decimal.TryParse str with
+            | (true, d) -> YamlValue.Number d
+            | _ -> YamlValue.String str
+    
+    | :? IList<obj> as list ->
+        list
+        |> Seq.toArray
+        |> Array.map YamlValue.FromObjTree
+        |> YamlValue.Array
+    
+    | :? IDictionary<obj, obj> as dict -> raise (NotImplementedException())
