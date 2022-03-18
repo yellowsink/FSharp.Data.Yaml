498eb58313a922018c0152b479518e05e720c476
yellowsink <yellowsink@riseup.net>
2022-03-18 12:09:51 +0000

    more tree conversion
    more tree conversion

--- src/YamlValue.fs
+++ src/YamlValue.fs
@@ -6,10 +6,11 @@ open System.Collections.Generic
 [<RequireQualifiedAccess>]
 type YamlValue =
   | String of string
-  | Number of decimal
+  | Integer of int
+  | Decimal of decimal
   //| Float of float
-  | Record of properties:(string * YamlValue)[]
-  | Array of elements:YamlValue[]
+  | Record of properties:(YamlValue * YamlValue)[]
+  | Sequence of elements:YamlValue[]
   | Boolean of bool
   | Null
 
@@ -21,13 +22,27 @@ type YamlValue =
         | "false" -> YamlValue.Boolean false
         | _ ->
             match Decimal.TryParse str with
-            | (true, d) -> YamlValue.Number d
+            | (true, d) ->
+                match Int32.TryParse str with
+                // if no decimal places
+                | (true, i) when (decimal i) = d -> YamlValue.Integer i
+                | _ -> YamlValue.Decimal d
             | _ -> YamlValue.String str
     
     | :? IList<obj> as list ->
         list
         |> Seq.toArray
         |> Array.map YamlValue.FromObjTree
-        |> YamlValue.Array
+        |> YamlValue.Sequence
     
-    | :? IDictionary<obj, obj> as dict -> raise (NotImplementedException())
+    | :? IDictionary<obj, obj> as dict ->
+        dict
+        |> Seq.toArray
+        |> Array.map (fun pair ->
+            YamlValue.FromObjTree pair.Key, YamlValue.FromObjTree pair.Value
+            )
+        |> YamlValue.Record
+    
+    | null -> YamlValue.Null
+
+    | _ -> failwithf $"Invalid tree node {node} passed to %s{nameof YamlValue.FromObjTree}"
